0364920aa42d06c04ffce9b54aceed7fe543b61a
rBaun <runeb95@gmail.com>
2024-09-14 14:25:28 +0200

    Common Components
    - Added input components without styling

--- src/app/components/common/input-number/input-number.component.ts
+++ src/app/components/common/input-number/input-number.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-input-number',
-  standalone: true,
-  imports: [],
-  templateUrl: './input-number.component.html',
-  styleUrl: './input-number.component.scss'
+	selector: 'app-input-number',
+	standalone: true,
+	imports: [ReactiveFormsModule],
+	templateUrl: './input-number.component.html',
+	styleUrl: './input-number.component.scss',
 })
 export class InputNumberComponent {
-
+	@Input({ required: true }) formControl!: FormControl;
+	@Input() label = '';
+	@Input() placeholder = '';
+	@Input() disable = false;
+	@Input() lowestNumber = 0;
 }
--- src/app/components/common/input-percentage/input-percentage.component.ts
+++ src/app/components/common/input-percentage/input-percentage.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-input-percentage',
-  standalone: true,
-  imports: [],
-  templateUrl: './input-percentage.component.html',
-  styleUrl: './input-percentage.component.scss'
+	selector: 'app-input-percentage',
+	standalone: true,
+	imports: [ReactiveFormsModule],
+	templateUrl: './input-percentage.component.html',
+	styleUrl: './input-percentage.component.scss',
 })
 export class InputPercentageComponent {
-
+	@Input({ required: true }) formControl!: FormControl;
+	@Input() label = '';
+	@Input() placeholder = '';
+	@Input() disable = false;
+	@Input() lowestNumber = 0;
+	@Input() highestNumber = 100;
 }
