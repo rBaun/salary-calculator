cc281427390f83775fb3ffcf8afc176070e20a24
rBaun <runeb95@gmail.com>
2024-09-21 21:46:40 +0200

    #3 Container Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+	Component,
+	OnInit
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IconCogComponent } from './components/common/icon-cog/icon-cog.component';
+import { ContainerComponent } from './common/container/container.component';
+import { ContainerModel } from './common/container/models/container.model';
+import { DialogHostDirective } from './common/dialog/dialog-host.directive';
+import { DialogService } from './common/dialog/dialog.service';
+import { IconCogComponent } from './common/icon/icon-cog/icon-cog.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, IconCogComponent],
+	imports: [
+		RouterOutlet,
+		IconCogComponent,
+		CommonModule,
+		DialogHostDirective,
+		ContainerComponent,
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
-export class AppComponent {
-	protected settingsClick = () => {
-		this.onSettingsClicked();
-	};
+export class AppComponent implements OnInit {
+	protected containerInput!: ContainerModel;
 
-	private onSettingsClicked = () => {
-		console.log('Settings clicked');
+	constructor(public dialogService: DialogService) {}
+
+	ngOnInit(): void {
+		this.setupContainerInput();
+	}
+
+	private setupContainerInput = (): void => {
+		this.containerInput = {
+			title: 'Salary Calculator',
+			enableSettings: true,
+		};
 	};
 }
---
+++ src/app/common/container/container.component.ts
@@ -0,0 +1,41 @@
+import {
+	AfterViewInit,
+	Component,
+	Input,
+	ViewChild,
+	ViewContainerRef,
+} from '@angular/core';
+import { SettingsService } from '../../widgets/calculator-settings/settings.feature.service';
+import { IconComponent } from '../icon/icon.component';
+import { ContainerModel } from './models/container.model';
+
+@Component({
+	selector: 'app-container',
+	standalone: true,
+	imports: [IconComponent],
+	templateUrl: './container.component.html',
+	styleUrl: './container.component.scss',
+})
+export class ContainerComponent implements AfterViewInit {
+	@Input({ required: true }) data!: ContainerModel;
+
+	@ViewChild('dialogHost', { read: ViewContainerRef, static: true })
+	dialogHost!: ViewContainerRef;
+
+	constructor(private settingsService: SettingsService) {}
+
+	ngAfterViewInit(): void {
+		this.settingsService.setViewContainerRef(this.dialogHost);
+	}
+
+	protected settingsClick = (): void => {
+		this.settingsService
+			.openSettingsDialog(this.dialogHost)
+			.subscribe(result => {
+				console.log('Dialog result:', result);
+				if (result) {
+					// TODO: save settings
+				}
+			});
+	};
+}
