c24d388a007bcc2b5050ee016be7c355bfe0967c
rBaun <runeb95@gmail.com>
2024-09-27 21:31:45 +0200

    Minor changes
    - Added english translations for #2
    - Added default config file

---
+++ src/assets/config/default-config.ts
@@ -0,0 +1,5 @@
+import { SettingsConfig } from '../../app/widgets/calculator-settings/models/settings-config.model';
+
+export const defaultSettingsConfig = {
+	language: 'en_US',
+} as SettingsConfig;
